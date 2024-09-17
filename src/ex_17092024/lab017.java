a65b828a5297f821f5e567bb71fb975e0d1d9223
BCSIN-LPA-0057\Gursharan Kaur <gursharan.kaur@bcstechnology.com.au>
2024-09-17 16:55:08 +0530

    new class

---
+++ src/ex_17092024/lab017.java
@@ -0,0 +1,11 @@
+package ex_17092024;
+
+public class lab017 {
+    public static void main(String[] args) {
+        byte b =10;
+        byte c =10;
+        System.out.println(b+c);
+
+
+    }
+}
---
+++ src/ex_17092024/lab018.java
@@ -0,0 +1,20 @@
+package ex_17092024;
+
+public class lab018 {
+    public static void main(String[] args) {
+        short s = 10;
+
+        char c = 'B';
+
+        int ss = s+c;
+
+        System.out.println(ss);
+       // Character Arithmetic:
+
+//        In Java, characters are represented by their ASCII values. The ASCII value of 'B' is 66.
+//        Therefore, s + c is equivalent to 10 + 66, which results in 76.
+//        Output:
+//
+//        System.out.println(ss); prints the value of ss, which is 76.
+    }
+}
