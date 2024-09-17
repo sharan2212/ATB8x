a9d1dd9dc27362ba91da52d30c740f310b25bc6c
BCSIN-LPA-0057\Gursharan Kaur <gursharan.kaur@bcstechnology.com.au>
2024-09-17 15:25:32 +0530

    new class

---
+++ src/ex_13092024/Lab016.java
@@ -0,0 +1,10 @@
+package ex_13092024;
+//byte b = 10; long l = 10l; → How much Byte will be used.
+public class Lab016 {
+    public static void display() {
+        byte b = 10 ;//1 byte
+        long l= 10l; // 8 byte
+       // System.out.println("float value = " + f);
+    }
+}
+    //final--- keyword is used for keyword CONSTANT_NAME=VALUE
